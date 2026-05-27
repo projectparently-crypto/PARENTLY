3eebbca87adc624c2700c3cde4a1a06929afdbb1
Leydi-2027 <leydi.aparicio2027@adoc.superate.org.sv>
2026-05-27 10:51:35 -0600

    Navbar con error, arreglada

--- comunidades.php
+++ comunidades.php
@@ -25,20 +25,16 @@
     </button>
 
     <!-- Opciones (derecha) -->
-    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+  <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
       <ul class="navbar-nav mx-auto gap-2">
         <li class="nav-item">
           <a class="nav-link" href="recursos.php">Recursos</a>
         </li>
         <li class="nav-item">
-<<<<<<< HEAD
           <a class="nav-link" href="actividades.php">Actividades</a>
-=======
-          <a class="nav-link" href="">Actividades</a>
->>>>>>> cb0d0f8e0fa7bed32cad68af9b2621825699b42b
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="especialista.php">Especialistas</a>
+          <a class="nav-link" href="especialistas.php">Especialistas</a>
         </li>
          <li class="nav-item">
           <a class="nav-link" href="comunidades.php">Comunidades</a>
