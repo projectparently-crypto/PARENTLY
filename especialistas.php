3d0bc3d45542179ec6b58404392c1ec8d035aea8
Leydi-2027 <leydi.aparicio2027@adoc.superate.org.sv>
2026-05-26 22:30:23 -0600

    Paginas editadas para conexion

--- especialistas.php
+++ especialistas.php
@@ -89,7 +89,7 @@ $specialists = [
                 <li class="nav-item"><a class="nav-link" href="recursos.php">Recursos</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Actividades</a></li>
                 <li class="nav-item"><a class="nav-link active" href="especialistas.php">Especialistas</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Comunidades</a></li>
+                <li class="nav-item"><a class="nav-link" href="comunidades.php">Comunidades</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Contactanos</a></li>
             </ul>
         </div>
