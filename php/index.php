210c0a22f8764afabb425c389a2329910f16af07
Mirian-zly <mirian.zelaya2027@adoc.superate.org.sv>
2026-06-01 15:46:26 -0600

    cmbios

--- php/index.php
+++ php/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Parently</title>
-    <link rel="stylesheet" href="homepage.css">
+    <link rel="stylesheet" href=".../style/homepage.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
