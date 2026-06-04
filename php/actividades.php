c31f06f26e13b1456f27d7a54bfdba9f7ff38589
JesusAleman1525JD <eduardo.mendez2027@adoc.superate.org.sv>
2026-06-03 22:49:37 -0600

    arreglando navbar de actividades

--- php/actividades.php
+++ php/actividades.php
@@ -10,79 +10,62 @@ session_start();
     <title>Actividades</title>
 
 
-    </style>
-
-
-    <link rel="stylesheet" href="..style/navbar.css">
-
-    <link rel="stylesheet" href="../style/actividades.css">
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-</head>
-</head>
+
+    <link rel="stylesheet" href="../style/actividades.css">
+  </head>
 <body>
 
-    <!-- NAVBAR -->
 
-   <!-- NAVBAR -->
-<nav class="navbar navbar-expand-lg ">
-  <div class="container-fluid">
-
-    <!-- Logo + nombre (izquierda) -->
-    <a class="navbar-brand d-flex align-items-center" href="index.php">
+  <!-- NAVBAR -->
+    
+  <nav class="navbar navbar-expand-lg ">
+    <div class="container-fluid">
+      <a class="navbar-brand d-flex align-items-center" href="index.php">
       <img src="../photos/ChatGPT_Image_May_3__2026__07_29_09_PM-removebg-preview.png" width="50" class="me-3">
-      Parently
-    </a>
-
-    <!-- Botón responsive -->
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <!-- Opciones (derecha) -->
+        Parently
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
     <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-      <ul class="navbar-nav mx-auto gap-2">
-        <li class="nav-item">
-          <a class="nav-link" href="recursos.php">Recursos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="actividades.php">Actividades</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="especialistas.php">Especialistas</a>
-        </li>
-         <li class="nav-item">
-          <a class="nav-link" href="comunidades.php">Comunidades</a>
-        </li>
-         <li class="nav-item">
-          <a class="nav-link" href="contactanos.php">Contactanos</a>
-        </li>
-      </ul>
-      
-      <!-- Botones - Con sesión o sin sesión -->
-      <div class="d-flex gap-2 align-items-center">
-        <?php if (isset($_SESSION["usuario_nombre"])): ?>
-          <!-- Usuario logueado -->
-          <div class="profile-btn d-flex align-items-center gap-2">
-            <a href="perfil.php" class="avatar-link">
-              <div class="avatar-small">
-                <?php echo strtoupper(substr($_SESSION["usuario_nombre"], 0, 1)); ?>
-              </div>
-            </a>
-            <a href="perfil.php" class="profile-name"><?php echo htmlspecialchars($_SESSION["usuario_nombre"]); ?></a>
-            <a href="logout.php" class="btn btn-danger btn-sm">Cerrar Sesión</a>
-          </div>
-        <?php else: ?>
-          <!-- Usuario sin sesión -->
-          <a href="login.php" class="btn btn-outline-success">Iniciar Sesión</a>
-          <a href="registro.php" class="btn btn-success">Registrarse</a>
-        <?php endif; ?>
+        <ul class="navbar-nav mx-auto gap-2">
+          <li class="nav-item">
+            <a class="nav-link" href="recursos.php">Recursos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="actividades.php">Actividades</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="especialistas.php">Especialistas</a>
+          </li>
+            <li class="nav-item">
+            <a class="nav-link" href="comunidades.php">Comunidades</a>
+          </li>
+            <li class="nav-item">
+            <a class="nav-link" href="contactanos.php">Contactanos</a>
+          </li>
+        </ul>
+        <div class="d-flex gap-2 align-items-center">
+          <?php if (isset($_SESSION["usuario_nombre"])): ?>
+            <div class="profile-btn d-flex align-items-center gap-2">
+              <a href="perfil.php" class="avatar-link">
+                <div class="avatar-small">
+                  <?php echo strtoupper(substr($_SESSION["usuario_nombre"], 0, 1)); ?>
+                </div>
+              </a>
+              <a href="perfil.php" class="profile-name"><?php echo htmlspecialchars($_SESSION["usuario_nombre"]); ?></a>
+              <a href="logout.php" class="btn btn-danger btn-sm">Cerrar Sesión</a>
+            </div>
+          <?php else: ?>
+            <a href="login.php" class="btn btn-outline-success">Iniciar Sesión</a>
+            <a href="registro.php" class="btn btn-success">Registrarse</a>
+          <?php endif; ?>
+        </div>
       </div>
     </div>
-
-  </div>
-</nav>
+  </nav>
 
     <!-- HERO -->
 
