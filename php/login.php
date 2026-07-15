973ed721161f5990229d29a21f7ac7a81ce57fdd
Aleerivas <alejandra.sibrian2027@adoc.superate.org.sv>
2026-07-15 10:30:11 -0600

    login funcional

--- php/login.php
+++ php/login.php
@@ -46,7 +46,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                 $stmt->close();
                 $conn->close();
-                header("Location: index.php");
+                header("Location: ../index.php");
                 exit();
             } else {
                 $error = "Contraseña incorrecta.";
