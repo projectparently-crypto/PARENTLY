0dd997f4d10abe41d94f952d26613bd954765e4b
Stev71015 <steven.ardon2027@adoc.superate.org.sv>
2026-07-22 10:45:26 -0600

    arreglando direcciones

--- php/foro.php
+++ php/foro.php
@@ -405,17 +405,17 @@ $id = $_GET["id"] ?? 1;
       <div class="d-flex gap-2 align-items-center">
         <?php if (isset($_SESSION["usuario_nombre"])): ?>
           <div class="profile-btn d-flex align-items-center gap-2">
-            <a href="php/perfil.php" class="avatar-link">
+            <a href="../php/perfil.php" class="avatar-link">
               <div class="avatar-small">
                 <?php echo strtoupper(substr($_SESSION["usuario_nombre"], 0, 1)); ?>
               </div>
             </a>
-            <a href="php/perfil.php" class="profile-name"><?php echo htmlspecialchars($_SESSION["usuario_nombre"]); ?></a>
-            <a href="php/logout.php" class="btn btn-danger btn-sm">Cerrar Sesión</a>
+            <a href="../php/perfil.php" class="profile-name"><?php echo htmlspecialchars($_SESSION["usuario_nombre"]); ?></a>
+            <a href="../php/logout.php" class="btn btn-danger btn-sm">Cerrar Sesión</a>
           </div>
         <?php else: ?>
-          <a href="login.php" class="btn btn-outline-success">Iniciar Sesión</a>
-          <a href="registro.php" class="btn btn-success">Registrarse</a>
+          <a href="../login.php" class="btn btn-outline-success">Iniciar Sesión</a>
+          <a href="../registro.php" class="btn btn-success">Registrarse</a>
         <?php endif; ?>
       </div>
     </div>
